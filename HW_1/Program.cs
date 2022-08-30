c6734d8344c95248d2e9c5b36e76a32fad8468e3
insomnea42 <khveroe@gmail.com>
2022-08-30 17:19:49 +0500

    решение дз

---
+++ HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите длину массива M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[m];
+int chislo = 0;
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < m; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] > 0) chislo++;
+}
+Console.WriteLine("Количество элементов больше нуля: " + chislo);
---
+++ HW_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double y, x;
+double a = b1 - b2;
+double b = k2 - k1;
+
+if (b != 0)
+{
+    x = a / b;
+    y = k1 * x + b1;
+
+    Console.WriteLine("Прямые пересекаются в точке (" + x + "; " + y + ")");
+}
+else Console.WriteLine("Просьба проверить значения, нет возможности делить на 0");
